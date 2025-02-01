45c1629b7f298aee503012da4687e44573edb2b6
cattyn <cvenby@gmail.com>
2025-02-01 03:45:00 +0300

    added early color object support

--- catformat-core/src/main/java/dev/cattyn/catformat/stylist/Stylist.java
+++ catformat-core/src/main/java/dev/cattyn/catformat/stylist/Stylist.java
@@ -1,10 +1,16 @@
 package dev.cattyn.catformat.stylist;
 
 import dev.cattyn.catformat.formatter.FormatEntry;
+import dev.cattyn.catformat.stylist.wrappers.AwtWrapper;
+import dev.cattyn.catformat.stylist.wrappers.ColorWrapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Stylist<T> {
+    // TODO no. This way it sucks...
+    List<ColorWrapper<?>> WRAPPERS = new ArrayList<>(List.of(new AwtWrapper()));
+
     List<FormatEntry> getEntries(T target);
 
     default List<FormatEntry> getEntries0(Object object) {
