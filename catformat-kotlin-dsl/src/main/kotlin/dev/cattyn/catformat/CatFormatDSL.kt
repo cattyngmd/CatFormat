7c986885e3916e4b9ad491eca9541472bc1dbef2
cattyn <cvenby@gmail.com>
2025-02-04 23:52:24 +0300

    kotlin dsl stylist support

--- catformat-kotlin-dsl/src/main/kotlin/dev/cattyn/catformat/CatFormatDSL.kt
+++ catformat-kotlin-dsl/src/main/kotlin/dev/cattyn/catformat/CatFormatDSL.kt
@@ -1,12 +1,17 @@
 package dev.cattyn.catformat
 
 import dev.cattyn.catformat.entry.FormatEntry
+import dev.cattyn.catformat.stylist.Stylist
 import java.awt.Color
 import java.util.function.Supplier
 
 // the most useless shit ever
 
 class Config<T>(private val format: CatFormat<T>) {
+    var stylist: Stylist<*>
+        get() = format.entries().stylist()
+        set(value) = format.entries().stylist(value)
+
     infix fun String.by(color: Supplier<Int>) {
         format.entries().add(FormatEntry(this, color))
     }
