fde0fc627e194f6d7c316b6072a080dadb7b1b69
cattyn <cvenby@gmail.com>
2025-02-05 19:13:11 +0300

    probably fixed jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 include("catformat-legacy-text")
 include("catformat-fabric")
 include("catformat-core")
