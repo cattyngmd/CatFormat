44c7a0d20da45006679c9eba72b9735f85abf9ef
cattyn <cvenby@gmail.com>
2025-02-06 14:16:12 +0300

    added java plugin into build.gradle.kts

--- catformat-kotlin-dsl/build.gradle.kts
+++ catformat-kotlin-dsl/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("java")
 }
 
 repositories {
@@ -12,4 +13,8 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
 }
