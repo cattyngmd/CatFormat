1787d7bc44f67ef6c8f672c77b4d81e4f594578d
cattyn <cvenby@gmail.com>
2025-08-05 00:29:53 +0300

    codacy fix

--- version.py
+++ version.py
@@ -3,17 +3,18 @@ import re
 regex = r"[0-9]+([.][0-9]+)+"
 version = input()
 
-def replace_version(file_name, str, lower = True):
+def replace_version(file_name, find, lower = True):
     with open(file_name) as f:
         lines = []
 
         for line in f.readlines():
-            if str in (line.lower() if lower else line):
+            if find in (line.lower() if lower else line):
                 lines.append(re.sub(regex, version, line))
             else:
                 lines.append(line)
 
         open(file_name, "w").writelines(lines)
+
 
 if re.fullmatch(regex, version):
     replace_version("README.md", "implementation")
